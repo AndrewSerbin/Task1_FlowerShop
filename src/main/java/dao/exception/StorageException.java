d3738a1445a510f6daad681a50b2ed07f6b7b190
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-17 12:30:38 +0300

    Added storage exceptions.

---
+++ src/main/java/dao/exception/StorageException.java
@@ -0,0 +1,12 @@
+package dao.exception;
+
+public class StorageException extends Exception {
+
+    public StorageException() {
+	super();
+    }
+
+    public StorageException(String message) {
+	super(message);
+    }
+}
---
+++ src/main/java/dao/exception/StorageSystemException.java
@@ -0,0 +1,12 @@
+package dao.exception;
+
+public class StorageSystemException extends StorageException {
+
+    public StorageSystemException() {
+	super();
+    }
+
+    public StorageSystemException(String message) {
+	super(message);
+    }
+}
