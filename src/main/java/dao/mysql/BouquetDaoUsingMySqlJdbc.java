8825d1090bff169c4b838d2a12818f50fc32d2d6
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-20 22:19:47 +0300

    Changed get all bouquets realization.

--- src/main/java/dao/mysql/BouquetDaoUsingMySqlJdbc.java
+++ src/main/java/dao/mysql/BouquetDaoUsingMySqlJdbc.java
@@ -31,14 +31,16 @@ public class BouquetDaoUsingMySqlJdbc implements BouquetDao {
     public static final String LOGIN = "root";
     public static final String PASSWORD = "root";
 
-    public static final String SELECT_ALL_BOUQUETS = "select bouquets.*, " +
-	    "flowers.name as flower_name, flowers.price as flower_price, flowers.type as flower_type, " +
-	    "accessories.name as accessory_name, accessories.price as accessory_price, accessories.type as accessory_type\r\n" +
+    public static final String SELECT_ALL_BOUQUETS_AND_FLOWERS = "select bouquets.*, flowers.name as flower_name, " +
+	    "flowers.price as flower_price, flowers.type as flower_type\r\n" +
 	    "from bouquets\r\n" +
 	    "join bouquets_flowers\r\n" +
 	    "on bouquets_flowers.id_bouquets = bouquets.id\r\n" +
 	    "join flowers\r\n" +
-	    "on flowers.id = bouquets_flowers.id_flowers\r\n" +
+	    "on flowers.id = bouquets_flowers.id_flowers;";
+    public static final String SELECT_ALL_BOUQUETS_AND_ACCESSORIES = "select bouquets.*, accessories.name as accessory_name," +
+	    "accessories.price as accessory_price, accessories.type as accessory_type\r\n" +
+	    "from bouquets\r\n" +
 	    "join bouquets_accessories\r\n" +
 	    "on bouquets_accessories.id_bouquets = bouquets.id\r\n" +
 	    "join accessories\r\n" +
@@ -55,8 +57,16 @@ public class BouquetDaoUsingMySqlJdbc implements BouquetDao {
     @Override
     public List<Bouquet> getAll() throws StorageSystemException, IncompleteBouquetInitialization {
 	Map<Integer, BouquetBuilder> builders = new HashMap<>();
+
+	getBouquetsAndFlowers(builders);
+	getBouquetsAndAccessories(builders);
+
+	return getBouquetsFromBuildersMap(builders);
+    }
+
+    private void getBouquetsAndFlowers(Map<Integer, BouquetBuilder> builders) throws StorageSystemException {
 	try (Connection connection = getConnection();
-		PreparedStatement pStatement = connection.prepareStatement(SELECT_ALL_BOUQUETS);
+		PreparedStatement pStatement = connection.prepareStatement(SELECT_ALL_BOUQUETS_AND_FLOWERS);
 		ResultSet resultSet = pStatement.executeQuery()) {
 	    BouquetBuilder builder = new BouquetBuilder();
 	    while (resultSet.next()) {
@@ -68,14 +78,37 @@ public class BouquetDaoUsingMySqlJdbc implements BouquetDao {
 			    .setSize(Size.valueOf(resultSet.getString("size")))
 			    .setStemLength(StemLength.valueOf(resultSet.getString("stem_length")))
 			    .addFlower(new Flower(resultSet.getString("flower_name"), resultSet.getInt("flower_price"),
-				    FlowerType.valueOf(resultSet.getString("flower_type"))))
+				    FlowerType.valueOf(resultSet.getString("flower_type"))));
+		    builders.put(resultSet.getInt("id"), builder);
+		} else {
+		    builders.get(resultSet.getInt("id"))
+			    .addFlower(new Flower(resultSet.getString("flower_name"), resultSet.getInt("flower_price"),
+				    FlowerType.valueOf(resultSet.getString("flower_type"))));
+		}
+	    }
+	} catch (SQLException e) {
+	    throw new StorageSystemException();
+	}
+    }
+
+    private void getBouquetsAndAccessories(Map<Integer, BouquetBuilder> builders) throws StorageSystemException {
+	try (Connection connection = getConnection();
+		PreparedStatement pStatement = connection.prepareStatement(SELECT_ALL_BOUQUETS_AND_ACCESSORIES);
+		ResultSet resultSet = pStatement.executeQuery()) {
+	    BouquetBuilder builder = new BouquetBuilder();
+	    while (resultSet.next()) {
+		if (!builders.containsKey(resultSet.getInt("id"))) {
+		    builder = new BouquetBuilder()
+			    .setName(resultSet.getString("name"))
+			    .setEvent(Event.valueOf(resultSet.getString("event")))
+			    .setFreshness(Freshness.valueOf(resultSet.getString("freshness")))
+			    .setSize(Size.valueOf(resultSet.getString("size")))
+			    .setStemLength(StemLength.valueOf(resultSet.getString("stem_length")))
 			    .addAccessory(new Accessory(resultSet.getString("accessory_name"), resultSet.getInt("accessory_price"),
 				    AccessoryType.valueOf(resultSet.getString("accessory_type"))));
 		    builders.put(resultSet.getInt("id"), builder);
 		} else {
 		    builders.get(resultSet.getInt("id"))
-			    .addFlower(new Flower(resultSet.getString("flower_name"), resultSet.getInt("flower_price"),
-				    FlowerType.valueOf(resultSet.getString("flower_type"))))
 			    .addAccessory(new Accessory(resultSet.getString("accessory_name"), resultSet.getInt("accessory_price"),
 				    AccessoryType.valueOf(resultSet.getString("accessory_type"))));
 		}
@@ -83,8 +116,6 @@ public class BouquetDaoUsingMySqlJdbc implements BouquetDao {
 	} catch (SQLException e) {
 	    throw new StorageSystemException();
 	}
-
-	return getBouquetsFromBuildersMap(builders);
     }
 
     private List<Bouquet> getBouquetsFromBuildersMap(Map<Integer, BouquetBuilder> builders) throws IncompleteBouquetInitialization {
