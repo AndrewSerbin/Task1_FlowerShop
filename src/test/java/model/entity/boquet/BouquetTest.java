5f7d9e60dabede41dd34c50dc3cbef35e878d863
AndrewSerbin <andrewserbin01@gmail.com>
2017-08-21 20:53:51 +0300

    Added enum stub to the test.

--- src/test/java/model/entity/boquet/BouquetTest.java
+++ src/test/java/model/entity/boquet/BouquetTest.java
@@ -5,25 +5,51 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-import dao.BouquetDao;
-import dao.exception.StorageSystemException;
-import dao.stub.BouquetDaoUsingEnumStub;
+import model.entity.boquet.builder.BouquetBuilder;
+import model.entity.boquet.component.plant.Flower;
+import model.entity.boquet.component.plant.FlowerType;
 import model.entity.boquet.exception.IncompleteBouquetInitialization;
 
 public class BouquetTest {
 
-    Bouquet boquet;
+    private Bouquet boquet;
+
+    enum BouquetStub {
+
+	BOUQUET_1("Boquet 1", Event.ALL, Freshness.NORMALLY, Size.SMALL, StemLength.MEDIUM, "Rose", 100,
+		FlowerType.ONE_FLOWER, false, true), BOUQUET_2("Boquet 2", Event.ALL, Freshness.BAD,
+			Size.SMALL, StemLength.LONG, "Rose", 100, FlowerType.ONE_FLOWER, false, true);
+
+	private Bouquet bouquet;
+
+	BouquetStub(String name, Event event, Freshness freshness, Size size, StemLength stemLength,
+		String flowerName, int flowerPrice, FlowerType flowerType, boolean isFeed, boolean isSupport) {
+	    try {
+		bouquet = new BouquetBuilder()
+			.setName(name)
+			.setEvent(event)
+			.setFreshness(freshness)
+			.setSize(size)
+			.setStemLength(stemLength)
+			.addFlower(new Flower(flowerName, flowerPrice, flowerType, isFeed, isSupport))
+			.build();
+	    } catch (IncompleteBouquetInitialization e) {
+		e.printStackTrace();
+	    }
+	}
+
+	public Bouquet getBouquet() {
+	    return bouquet;
+	}
+
+	public void setBouquet(Bouquet bouquet) {
+	    this.bouquet = bouquet;
+	}
+    }
 
     @Before
     public void init() {
-	BouquetDao dao = new BouquetDaoUsingEnumStub();
-	try {
-	    boquet = dao.getAll().get(0);
-	} catch (StorageSystemException e) {
-	    e.printStackTrace();
-	} catch (IncompleteBouquetInitialization e) {
-	    e.printStackTrace();
-	}
+	boquet = BouquetStub.BOUQUET_1.getBouquet();
     }
 
     @Test
